6550320ddd99a45b4858e372fb4b7760608c5fd0
production-jeu <43345835+production-jeu@users.noreply.github.com>
2019-02-22 16:53:51 -0500

    text resize

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function(){
     console.log('oui animation');
-    document.getElementById("main").innerHTML += "<marquee style='font-size: 20px; color:red;'>Oui animation</marquee>";
+    document.getElementById("main").innerHTML += "<marquee style='font-size: 40px; color:red;'>Oui animation</marquee>";
 })
