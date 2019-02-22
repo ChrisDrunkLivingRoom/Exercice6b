c484a69752a139953a97ef1806c7b87446e6c110
production-jeu <43345835+production-jeu@users.noreply.github.com>
2019-02-22 15:35:41 -0500

    animation oui animation

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function(){
-
     console.log('oui animation');
+    document.getElementById("main").innerHTML += "<marquee style='font-size: 20px;'>Oui animation</marquee>";
 })
